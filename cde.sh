727d0bc71dba3b8d8bd9964cccdfe918bc306860
Kelly Christus <kellimus@gmail.com>
2022-01-14 15:32:48 -0700

    Update cde.sh

--- cde.sh
+++ cde.sh
@@ -5,7 +5,7 @@ then
 	sudo apt install autoconf automake libtool git build-essential g++ 	lib{xt,xmu,xft,xinerama,xpm,pam,motif,ssl,xaw7,x11,xss,tirpc,jpeg,freetype6,utempter}-dev tcl-dev ksh m4 ncompress xfonts-{100,75}dpi{,-transcoded} rpcbind bison patch xbitmaps x11proto-fonts-dev flex
 	cd $HOME/Documents/
 	echo -e "\033[4;38;5;178mCloning the CDE repo\033[0m\n"
-	echo -e "";
+	echo -e ""
 	git clone https://git.code.sf.net/p/cdesktopenv/code cdesktopenv-code
 	cd $HOME/Documents/cdesktopenv-code/cde/
 	echo -e "\033[1;3;38;5;165mRunning autogeneration, configuration, and installation\033[0m\n"
